2c3042a1ee29959306bd7e9c4023a440048bb260
johbenny <92914979+johbenny@users.noreply.github.com>
2024-05-26 14:17:54 +0800

    rectify code error

--- iticanwrapper/itican_py_wrapper.py
+++ iticanwrapper/itican_py_wrapper.py
@@ -101,7 +101,7 @@ initialization_error = "this instance is not acquired from ITICANChannel's stati
 class ITICANChannel:
 
     def __init__(self, pointer):
-        self.chn_pointer = pointer
+        self._chn_pointer = pointer
         self._inner_flag = False
 
     @staticmethod
@@ -202,7 +202,7 @@ class ITICANChannel:
         mode_param = ctypes.c_int32(mode_)
 
         # 发起dll调用
-        result = importDLL.dll.openChannel(self.chn_pointer, type_param, mode_param)
+        result = importDLL.dll.openChannel(self._chn_pointer, type_param, mode_param)
 
         return result
 
@@ -215,7 +215,7 @@ class ITICANChannel:
         """
         if not self._inner_flag:
             raise TypeError(initialization_error)
-        result = importDLL.dll.closeChannel(self.chn_pointer)
+        result = importDLL.dll.closeChannel(self._chn_pointer)
         return result
 
     def get_channel_name(self, chn_name_output: list):
@@ -229,7 +229,7 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         chn_name_output.clear()
         str_temp = ctypes.create_string_buffer(500)
-        result = importDLL.dll.getChannelName(self.chn_pointer, str_temp)
+        result = importDLL.dll.getChannelName(self._chn_pointer, str_temp)
         chn_name = str_temp.value.decode('utf-8')
         chn_name_output.append(chn_name)
         return result
@@ -244,7 +244,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         baud_rate_ = ctypes.c_uint64(baud_rate)
-        result = importDLL.dll.setBaudRate(self.chn_pointer, baud_rate_)
+        result = importDLL.dll.setBaudRate(self._chn_pointer, baud_rate_)
         return result
 
     def set_custom_baud_rate(self, brp: int, ts1: int, ts2: int, sjw: int):
@@ -280,7 +280,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         baud_rate_ = ctypes.c_uint64(0)
-        result = importDLL.dll.getBaudRate(self.chn_pointer, ctypes.byref(baud_rate_))
+        result = importDLL.dll.getBaudRate(self._chn_pointer, ctypes.byref(baud_rate_))
         if result == 0:
             baud_rate_output.clear()
             baud_rate_output.append(baud_rate_.value)
@@ -296,7 +296,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         baud_rate_ = ctypes.c_uint64(baud_rate_input)
-        result = importDLL.dll.setFdBaudRate(self.chn_pointer, baud_rate_)
+        result = importDLL.dll.setFdBaudRate(self._chn_pointer, baud_rate_)
         return result
 
     def set_custom_fd_baud_rate(self, brp: int, ts1: int, ts2: int, sjw: int, tdc_o: int):
@@ -340,7 +340,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         baud_rate_ = ctypes.c_uint64(0)
-        result = importDLL.dll.getBaudRate(self.chn_pointer, ctypes.byref(baud_rate_))
+        result = importDLL.dll.getBaudRate(self._chn_pointer, ctypes.byref(baud_rate_))
         if result == 0:
             baud_rate_output.clear()
             baud_rate_output.append(baud_rate_.value)
@@ -356,7 +356,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         str_temp = ctypes.create_string_buffer(500)
-        result = importDLL.dll.getCustomBaudRate(self.chn_pointer, str_temp)
+        result = importDLL.dll.getCustomBaudRate(self._chn_pointer, str_temp)
         if result == 0:
             baud_rate_ = str_temp.value.decode('utf-8')
             custom_baud_rate_str_output.append(baud_rate_)
@@ -378,7 +378,7 @@ class ITICANChannel:
         data_array_ = (ctypes.c_uint8 * len(one_message.data_))(*(one_message.data_))
         date_length_ = ctypes.c_uint8(len(one_message.data_))
         timeout_ = ctypes.c_int32(timeout)
-        result = importDLL.dll.setMessage(self.chn_pointer, can_id_, message_type_, is_extended_, data_array_,
+        result = importDLL.dll.setMessage(self._chn_pointer, can_id_, message_type_, is_extended_, data_array_,
                                           date_length_,
                                           timeout_)
         return result
@@ -394,7 +394,6 @@ class ITICANChannel:
         """
         if not self._inner_flag:
             raise TypeError(initialization_error)
-        error_flag = False
         items_output.clear()
         counter = 0
         for can_msg in messages_container:
@@ -420,7 +419,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         count_temp = ctypes.c_int32(0)
-        result = importDLL.dll.getMessageCount(self.chn_pointer, ctypes.byref(count_temp))
+        result = importDLL.dll.getMessageCount(self._chn_pointer, ctypes.byref(count_temp))
         if result == 0:
             val = count_temp.value
             message_count_output.clear()
@@ -446,7 +445,7 @@ class ITICANChannel:
         data_l_temp = ctypes.c_uint8(0)
         timeout_temp = ctypes.c_int32(timeout)
 
-        result = importDLL.dll.getMessage(self.chn_pointer, ctypes.byref(id_temp), ctypes.byref(message_type_temp),
+        result = importDLL.dll.getMessage(self._chn_pointer, ctypes.byref(id_temp), ctypes.byref(message_type_temp),
                                           ctypes.byref(can_extended_temp), ctypes.byref(transmitted_temp),
                                           ctypes.byref(timestamp_temp), data_temp, ctypes.byref(data_l_temp),
                                           timeout_temp)
@@ -457,7 +456,6 @@ class ITICANChannel:
                 CANMessage(id_temp.value, MessageType(message_type_temp.value), can_extended_temp.value, data_list[:]))
         return result
 
-    def get_messages(self, messages_container: list, items_in_output: list, timeout: int = 0):
     def get_messages(self, messages_container: list, items: int, timeout: int = 0):
         """
         读取多条 CAN 消息
@@ -472,16 +470,13 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         messages_container.clear()
         counter = 0
-        for i in range(items_in_output[0]):
+        for i in range(items):
             one_message = []
             rt_val = self.get_message(one_message, timeout)
             if rt_val == 0:
                 messages_container.append(one_message[0])
-                counter += 1
             else:
-                items_in_output[0] = counter
                 return rt_val
-        items_in_output[0] = counter
         return 0
 
     def check_if_termination_supported(self, check_result: list):
@@ -497,7 +492,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         supported_temp = ctypes.c_uint8(0)
-        result = importDLL.dll.isTerminationSupported(self.chn_pointer, ctypes.byref(supported_temp))
+        result = importDLL.dll.isTerminationSupported(self._chn_pointer, ctypes.byref(supported_temp))
         if result == 0:
             check_result.clear()
             check_result.append(supported_temp.value != 0)
@@ -517,9 +512,9 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         result = 0
         if enable:
-            result = importDLL.dll.setTermination(self.chn_pointer, ctypes.c_uint8(1))
+            result = importDLL.dll.setTermination(self._chn_pointer, ctypes.c_uint8(1))
         else:
-            result = importDLL.dll.setTermination(self.chn_pointer, ctypes.c_uint8(0))
+            result = importDLL.dll.setTermination(self._chn_pointer, ctypes.c_uint8(0))
         return result
 
     def check_if_termination_enabled(self, enabled_output: list):
@@ -536,7 +531,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         enabled_temp = ctypes.c_uint8(0)
-        result = importDLL.dll.isTerminationEnabled(self.chn_pointer, ctypes.byref(enabled_temp))
+        result = importDLL.dll.isTerminationEnabled(self._chn_pointer, ctypes.byref(enabled_temp))
         if result == 0:
             enabled_output.clear()
             enabled_output.append(enabled_temp.value != 0)
@@ -555,7 +550,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         supported_temp = ctypes.c_uint8(0)
-        result = importDLL.dll.isEchoMessageSupported(self.chn_pointer, ctypes.byref(supported_temp))
+        result = importDLL.dll.isEchoMessageSupported(self._chn_pointer, ctypes.byref(supported_temp))
         if result == 0:
             check_result.clear()
             check_result.append(supported_temp.value != 0)
@@ -575,9 +570,9 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         result = 0
         if enable:
-            result = importDLL.dll.setTermination(self.chn_pointer, ctypes.c_uint8(1))
+            result = importDLL.dll.setTermination(self._chn_pointer, ctypes.c_uint8(1))
         else:
-            result = importDLL.dll.setTermination(self.chn_pointer, ctypes.c_uint8(0))
+            result = importDLL.dll.setTermination(self._chn_pointer, ctypes.c_uint8(0))
         return result
 
     def check_if_echo_message_enabled(self, check_result: list):
@@ -593,7 +588,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         enabled_temp = ctypes.c_uint8(0)
-        result = importDLL.dll.isEchoMessageEnabled(self.chn_pointer, ctypes.byref(enabled_temp))
+        result = importDLL.dll.isEchoMessageEnabled(self._chn_pointer, ctypes.byref(enabled_temp))
         if result == 0:
             check_result.clear()
             check_result.append(enabled_temp.value != 0)
@@ -613,9 +608,9 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         result = 0
         if enable:
-            result = importDLL.dll.setBusErrorReport(self.chn_pointer, ctypes.c_uint8(1))
+            result = importDLL.dll.setBusErrorReport(self._chn_pointer, ctypes.c_uint8(1))
         else:
-            result = importDLL.dll.setBusErrorReport(self.chn_pointer, ctypes.c_uint8(0))
+            result = importDLL.dll.setBusErrorReport(self._chn_pointer, ctypes.c_uint8(0))
         return result
 
     def apply_settings(self, temporary: bool):
@@ -632,9 +627,9 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         result = 0
         if temporary:
-            result = importDLL.dll.applySettings(self.chn_pointer, ctypes.c_uint8(1))
+            result = importDLL.dll.applySettings(self._chn_pointer, ctypes.c_uint8(1))
         else:
-            result = importDLL.dll.applySettings(self.chn_pointer, ctypes.c_uint8(0))
+            result = importDLL.dll.applySettings(self._chn_pointer, ctypes.c_uint8(0))
         return result
 
     def check_if_tx_mode_supported(self, mode: TxMode, check_result):
@@ -649,7 +644,7 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         mode_temp = ctypes.c_uint8(mode.value)
         supported_temp = ctypes.c_uint8(0)
-        result = importDLL.dll.isTxModeSupported(self.chn_pointer, mode_temp, ctypes.byref(supported_temp))
+        result = importDLL.dll.isTxModeSupported(self._chn_pointer, mode_temp, ctypes.byref(supported_temp))
         if result == 0:
             check_result.clear()
             check_result.append(supported_temp.value != 0)
@@ -668,7 +663,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         mode_temp = ctypes.c_uint8(mode.value)
-        result = importDLL.dll.setTxMode(self.chn_pointer, mode_temp)
+        result = importDLL.dll.setTxMode(self._chn_pointer, mode_temp)
         return result
 
     def set_tx_timing(self, can_id, period):
@@ -686,7 +681,7 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         can_id_temp = ctypes.c_uint32(can_id)
         period_temp = ctypes.c_int32(period)
-        result = importDLL.dll.setTxTiming(self.chn_pointer, can_id_temp, period_temp)
+        result = importDLL.dll.setTxTiming(self._chn_pointer, can_id_temp, period_temp)
         return result
 
     def check_if_blink_supported(self, check_result: list):
@@ -702,7 +697,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         supported_temp = ctypes.c_uint8(0)
-        result = importDLL.dll.isBlinkSupported(self.chn_pointer, ctypes.byref(supported_temp))
+        result = importDLL.dll.isBlinkSupported(self._chn_pointer, ctypes.byref(supported_temp))
         if result == 0:
             check_result.clear()
             check_result.append(supported_temp.value != 0)
@@ -722,9 +717,9 @@ class ITICANChannel:
             raise TypeError(initialization_error)
         result = 0
         if enable:
-            result = importDLL.dll.blinkChannel(self.chn_pointer, ctypes.c_uint8(1))
+            result = importDLL.dll.blinkChannel(self._chn_pointer, ctypes.c_uint8(1))
         else:
-            result = importDLL.dll.blinkChannel(self.chn_pointer, ctypes.c_uint8(0))
+            result = importDLL.dll.blinkChannel(self._chn_pointer, ctypes.c_uint8(0))
         return result
 
     def check_if_channel_blinking(self, check_result: list):
@@ -740,7 +735,7 @@ class ITICANChannel:
         if not self._inner_flag:
             raise TypeError(initialization_error)
         status = ctypes.c_uint8(0)
-        result = importDLL.dll.isChannelBlinking(self.chn_pointer, ctypes.byref(status))
+        result = importDLL.dll.isChannelBlinking(self._chn_pointer, ctypes.byref(status))
         if result == 0:
             check_result.clear()
             check_result.append(status.value != 0)
@@ -771,7 +766,6 @@ if __name__ == '__main__':
     err_spec = []
     rrt = ITICANChannel.get_last_error(-189, err_spec)
     cc = ITICANChannel(1)
-    cc.instance_test()
 
     print()
     print(chn_index_s)
