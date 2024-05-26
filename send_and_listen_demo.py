0895b6826c0b506e5681bd12965d6d3c5b168e7b
johbenny <92914979+johbenny@users.noreply.github.com>
2024-05-26 15:05:16 +0800

    add gitignore

--- send_and_listen_demo.py
+++ send_and_listen_demo.py
@@ -1,7 +1,4 @@
-# 这是一个示例 Python 脚本。
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 import time
 import threading
 
@@ -80,4 +77,3 @@ if __name__ == '__main__':
     print(chn_index_s)
     print(chn_count)
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
